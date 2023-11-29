12e6a9fe7da17fad20b9549592907e2baa6bcace
Victor Dias <diasvm@hotmail.com>
2023-11-28 20:39:19 -0800

    Week2 - Exercise: Icons, Favorites, and Comments - Final.

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -7,7 +7,8 @@ const CampsiteInfoScreen = ({ route }) => {
   const { campsite } = route.params;
 
   const [comments, setComments] = useState(COMMENTS);
-
+  const [favorite, setFavorite] = useState(false);
+ 
   const renderCommentItem = ({ item }) => {
     return (
       <View style={StyleSheet.commentItem}>
@@ -31,7 +32,11 @@ const CampsiteInfoScreen = ({ route }) => {
       }}
       ListHeaderComponent={
         <>
-          <RenderCampsite campsite={campsite} />
+          <RenderCampsite 
+              campsite={campsite} 
+              isFavorite={favorite}
+              markFavorite={() => setFavorite(true)}
+          />
           <Text style={styles.commentsTitle}>Comments</Text>
         </>
       }
