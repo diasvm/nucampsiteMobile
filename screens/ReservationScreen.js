e8486bf867540982836ef705d321a949c9b77949
Victor Dias <diasvm@hotmail.com>
2023-12-16 13:39:21 -0800

    Exercise: Local Notifications - Final.

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -4,6 +4,7 @@ import { Picker } from '@react-native-picker/picker';
 import DateTimePicker  from '@react-native-community/datetimepicker';
 import * as Animatable from "react-native-animatable";
 import RenderCampsite from "../features/campsites/RenderCampsite"
+import * as Notifications from 'expo-notifications'
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
@@ -33,6 +34,7 @@ const ReservationScreen = () => {
             {
               text: "OK",
               onPress: async () => {
+                presentLocalNotification(date.toLocaleDateString('en-US'));
                 await resetForm();
               },
             },
@@ -49,7 +51,36 @@ const ReservationScreen = () => {
         setHikeIn(false);
         setDate(new Date());
         setShowCalendar(false);
-    }
+    };
+
+    const presentLocalNotification = async (reservationDate) => {
+      const sendNotification = () => {
+        Notifications.setNotificationHandler({
+            handleNotification: async () => ({
+                shouldShowAlert: true,
+                shouldPlaySound: true,
+                shouldSetBadge: true
+            })
+          });
+          
+          Notifications.scheduleNotificationAsync({
+            content: {
+              title: "Your Campsite Reservation Search",
+              body: `Search for ${reservationDate} requested`,
+            },
+            trigger: null
+          });
+      };
+
+      let permissions = await Notifications.getPermissionsAsync();
+      if(!permissions.granted) {
+        permissions = await Notifications.requestPermissionsAsync();
+      }
+      if (permissions.granted) {
+        sendNotification();
+      }
+    };
+
 
       const handleShowCommentForm = () => {
         console.log("Showing comment form");
